26d792c3feee01258021edb5d3d4401e6d634b07
benjamink <benjamink@mirageoscience.com>
2024-03-07 14:52:28 -0800

    Test replicate function and add second plate to run test

--- tests/runtest/driver_test.py
+++ tests/runtest/driver_test.py
@@ -83,8 +83,8 @@ def get_input_file(filepath: Path) -> InputFile:
         ifile.set_data_value("dip_length", 300.0)
         ifile.set_data_value("dip", 65.0)
         ifile.set_data_value("dip_direction", 65.0)
-        ifile.set_data_value("number", 1)
-        ifile.set_data_value("spacing", 0.0)
+        ifile.set_data_value("number", 2)
+        ifile.set_data_value("spacing", 400.0)
 
     return ifile
 
@@ -105,7 +105,7 @@ def test_plate_simulation(tmp_path):
             k.name in [f"Iteration_0_{i}" for i in "xyz"] for k in data.property_groups
         )
         assert all(len(k.properties) == 20 for k in data.property_groups)
-        assert mesh.n_cells == 13253
+        assert mesh.n_cells == 14751
         assert len(np.unique(model.values)) == 4
         assert all(
             k in np.unique(model.values) for k in [1.0 / 7500, 1.0 / 2000, 1.0 / 20]
--- tests/utils/utils_test.py
+++ tests/utils/utils_test.py
@@ -6,8 +6,10 @@
 #
 
 import numpy as np
+from geoh5py import Workspace
+from geoh5py.objects import Surface
 
-from plate_simulation.utils import azimuth_to_unit_vector
+from plate_simulation.utils import azimuth_to_unit_vector, replicate
 
 
 def test_azimuth_to_unit_vector():
@@ -16,3 +18,32 @@ def test_azimuth_to_unit_vector():
     assert np.allclose(azimuth_to_unit_vector(180.0), np.array([0.0, -1.0, 0.0]))
     assert np.allclose(azimuth_to_unit_vector(270.0), np.array([-1.0, 0.0, 0.0]))
     assert np.allclose(azimuth_to_unit_vector(360.0), np.array([0.0, 1.0, 0.0]))
+
+
+def test_replicate_even(tmp_path):
+    workspace = Workspace(tmp_path / "test.geoh5")
+    surface = Surface.create(
+        workspace,
+        name="test",
+        vertices=np.array([[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0]]),
+        cells=np.array([[0, 1, 2], [0, 2, 3]]),
+    )
+    surfaces = replicate(surface, 2, 10.0, 90.0, np.array([0.0, 0.0, 0.0]))
+    assert surfaces[0].name == "test offset 1"
+    assert np.allclose(surfaces[0].vertices.mean(axis=0), np.array([-5.0, 0.0, 0.0]))
+    assert surfaces[1].name == "test offset 2"
+    assert np.allclose(surfaces[1].vertices.mean(axis=0), np.array([5.0, 0.0, 0.0]))
+
+
+def test_replicate_odd(tmp_path):
+    workspace = Workspace(tmp_path / "test.geoh5")
+    surface = Surface.create(
+        workspace,
+        name="test",
+        vertices=np.array([[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0]]),
+        cells=np.array([[0, 1, 2], [0, 2, 3]]),
+    )
+    surfaces = replicate(surface, 3, 5.0, 0.0, np.array([0.0, 0.0, 0.0]))
+    assert np.allclose(surfaces[0].vertices.mean(axis=0), np.array([0.0, -5.0, 0.0]))
+    assert np.allclose(surfaces[1].vertices.mean(axis=0), np.array([0.0, 0.0, 0.0]))
+    assert np.allclose(surfaces[2].vertices.mean(axis=0), np.array([0.0, 5.0, 0.0]))

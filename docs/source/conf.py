9c0737c6d6d6b8252955e7d6ac74d20e2b1c1968
benjamink <benjamink@mirageoscience.com>
2024-04-09 09:13:28 -0700

    pre-commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'plate-simulation'
-copyright = '2024, Benjamin Kary'
-author = 'Benjamin Kary'
-release = '2024'
+project = "plate-simulation"
+copyright = "2024, Benjamin Kary"
+author = "Benjamin Kary"
+release = "2024"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
